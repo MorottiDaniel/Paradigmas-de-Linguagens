30a708578cd6a10bd871ce5bf18e99a14de744f8
Daniel Morotti Vieira <morottivieirad@gmail.com>
2026-08-04 21:21:49 -0300

    Aula00

---
+++ Aula00/main.ts
@@ -0,0 +1,16 @@
+console.log("Hello, World!");
+
+let nTabuada: number = 12;
+
+console.log("\n===============================")
+console.log("Tabuada do "+ nTabuada);
+for(let i=1; i<=10; i++){
+    console.log(nTabuada + " X " + i + " = " + nTabuada*i);
+}
+console.log("===============================")
+
+//Vaga real: LATAM / Senior Functional Infrastructure Engineer (TypeScript / Integrations)
+
+//Faixa salarial: R$ 3.771 para júnior, R$ 7.601 para pleno e R$ 17.322 para sênior
+
+//Paradigmas: Orientada a Objeto, Orientada a tipos, Reativa e Assíncrona
